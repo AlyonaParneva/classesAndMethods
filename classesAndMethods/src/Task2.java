3f8e83b5ee3c8067001266f14366366e66202c0b
AlyonaParneva <parneva.aa@gmail.com>
2025-05-15 15:28:28 +0300

    task2 - sum the last 2 numbers

---
+++ classesAndMethods/src/Task2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static int sumLastNums(int x) {
+        int lastDigit = x % 10;
+        int secondLastDigit = (x / 10) % 10;
+        return lastDigit + secondLastDigit;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите целое число (не менее двух знаков): ");
+        int x = scanner.nextInt();
+        int result = sumLastNums(x);
+        System.out.println("Сумма двух последних цифр: " + result);
+    }
+}

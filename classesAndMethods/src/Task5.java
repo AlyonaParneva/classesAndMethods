b3549e013a955cc5653919aa34610e2f0f4e0ebd
AlyonaParneva <parneva.aa@gmail.com>
2025-05-15 16:09:28 +0300

    task5 - number is 2Digits?

---
+++ classesAndMethods/src/Task5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Task5 {
+    public static boolean is2Digits(int x) {
+        return x >= 10 && x <= 99;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите положительное число: ");
+        int x = scanner.nextInt();
+        if (x <= 0) {
+            System.out.println("Ошибка: число должно быть положительным.");
+            return;
+        }
+        boolean result = is2Digits(x);
+        System.out.println("Результат - число двузначное? " + result);
+    }
+}

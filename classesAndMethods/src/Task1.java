94141e5502c7bd205c1fe94d450a7693aac9ef93
AlyonaParneva <parneva.aa@gmail.com>
2025-05-15 15:04:26 +0300

    task1 - fractional part

---
+++ classesAndMethods/src/Task1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static double fraction(double x) {
+        double fractional = x - (int) x;
+        return Math.round(fractional * 1000.0) / 1000.0;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        String input = scanner.nextLine();
+        double x = Double.parseDouble(input);
+        double result = fraction(x);
+        System.out.println("Дробная часть: " + result);
+    }
+}

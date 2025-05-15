80b633c433e4ecca4562d9397cb0f090c0d2123f
AlyonaParneva <parneva.aa@gmail.com>
2025-05-15 15:51:44 +0300

    task4 - number is positive?

---
+++ classesAndMethods/src/Task4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Task4 {
+    public static boolean isPositive(int x) {
+        return x > 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите целое число: ");
+        int x = scanner.nextInt();
+        boolean result = isPositive(x);
+        System.out.println("Результат - положительное число?  " + result);
+    }
+}
